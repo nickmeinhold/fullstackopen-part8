3c88be2b75080d1b8cacdc8a5b9c2963d3e50e11
Nick Meinhold <nick@enspyr.co>
2025-11-15 17:14:01 +1100

    Completed 8.11

--- frontend/src/components/Authors.jsx
+++ frontend/src/components/Authors.jsx
@@ -1,8 +1,15 @@
-import { useQuery } from "@apollo/client/react";
-import { ALL_AUTHORS } from "../../queries";
+import { useState } from "react";
+import { useQuery, useMutation } from "@apollo/client/react";
+import { ALL_AUTHORS, EDIT_AUTHOR } from "../../queries";
 
 const Authors = ({ show }) => {
+  const [name, setName] = useState("");
+  const [born, setBorn] = useState("");
+
   const result = useQuery(ALL_AUTHORS);
+  const [editAuthor] = useMutation(EDIT_AUTHOR, {
+    refetchQueries: [{ query: ALL_AUTHORS }],
+  });
 
   if (!show) {
     return null;
@@ -13,6 +20,19 @@ const Authors = ({ show }) => {
   }
 
   const authors = result.data.allAuthors;
+
+  const submit = async (event) => {
+    event.preventDefault();
+
+    await editAuthor({
+      variables: { name: name, setBornTo: parseInt(born) },
+    });
+
+    console.log("edit author...");
+
+    setBorn("");
+    setName("");
+  };
 
   return (
     <div>
@@ -33,6 +53,25 @@ const Authors = ({ show }) => {
           ))}
         </tbody>
       </table>
+      <h2>Set birthyear</h2>
+      <form onSubmit={submit}>
+        <div>
+          name
+          <input
+            value={name}
+            onChange={({ target }) => setName(target.value)}
+          />
+        </div>
+        <div>
+          born
+          <input
+            type="number"
+            value={born}
+            onChange={({ target }) => setBorn(target.value)}
+          />
+        </div>
+        <button type="submit">update author</button>
+      </form>
     </div>
   );
 };
